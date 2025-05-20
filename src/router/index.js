b767fdfd85e098b7c7aa71c072fcd60829e52638
Юрченко Александр <YurchenkoAS@empl.vvsu.ru>
2025-05-20 16:13:23 +1000

    qr-code

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter,
+    createWebHashHistory
+ } from "vue-router";
+
+import HomePage from "@/home/HomePage.vue";
+import RobotBuilder from "@/build/RobotBuilder.vue";
+import PartInfo from "@/parts/PartInfo.vue";
+import QRCode from "@/build/QRCode.vue"
+
+export default createRouter ({
+    history: createWebHashHistory(),
+    routes: [{
+        path:'/',
+        name: 'ГЛАВНАЯ',
+        component: HomePage,
+    }, {
+        path: '/build',
+        name: 'ПОСТРОЙ БОТА',
+        component: RobotBuilder,
+    }, {
+        path: '/qrcode',
+        name: 'QR КОД',
+        component: QRCode,
+    },{
+        path: '/parts/:partType/:id',
+        name: 'ЗАПЧАСТИ',
+        component: PartInfo,
+    
+    }]
+});
